0d68ee8601acc22aab040ac34ec67bb7512a9d8d
Paweł Przybyła <pawel1996przybyla1996@outlook.com>
2023-11-04 09:14:43 +0100

    x

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 333");
+﻿
+
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! ");
+        Kielbasa();
+        Console.ReadLine(); //użytkownik musi wpisać coś na klawiaturze
+    }
+
+
+    private static void Kielbasa()
+    {
+    Console.WriteLine($"Kielbasa");
+    }
+
+}
+
+
+
+
+
---
+++ Projekt0/Projekt0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+namespace Projekt0
+{
+    class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
