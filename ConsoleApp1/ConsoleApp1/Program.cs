36407c11e866f828c47bd8ab6c22e4e0f3e758af
Paweł Przybyła <pawel1996przybyla1996@outlook.com>
2023-11-04 11:09:24 +0100

    Zatwierdzone

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,22 +4,39 @@ using System.Runtime.CompilerServices;
 
 internal class Program
 {
+
+    static int a=0;
+    static int b = 0;
+    static string? pobrana; // znak zapytania na końcu zmiennej możę również przypisać wartość null
     /// <summary>
     /// Procedura główna aplikacji
     /// </summary>
     /// <param name="args">Parametry wejściowe w postaci tablicy tekstowej</param>
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! ");
-        Kielbasa();
+        try
+        {
+            // a = a + 1; // albo i += 1
+            Console.WriteLine("Podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine()!);
+            //pobrana = Console.ReadLine();
+
+            Console.WriteLine("Podaj liczbę b: ");
+            b = int.Parse(Console.ReadLine()!);
+            //pobrana = Console.ReadLine();
+            Console.WriteLine(a + b);
+        }
+
+        catch (Exception)
+        {
+            Console.WriteLine("Stary podałeś coś nie tak");
+        }
+       
+
         Console.ReadLine(); //użytkownik musi wpisać coś na klawiaturze
     }
 
-
-    private static void Kielbasa()
-    {
-        Console.WriteLine($"Kielbasa");
-    }
+    
 
 }
 
