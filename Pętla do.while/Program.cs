321626ba309dcf4fee78b1ac1ad8db880b893c04
Paweł Przybyła <pawel1996przybyla1996@outlook.com>
2023-12-01 19:20:01 +0100

    gogo

---
+++ Pętla do.while/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Stwórz projekt w postaci aplikacji konsolowej.
+
+//Napisz program, który za pomocą instrukcji do ... while wyświetla liczby całkowite z przedziału od 1 do 20 podniesione do kwadratu. Wynik obliczeń wyświetl w konsoli.
+
+namespace Pętla_do.@while
+{
+    internal class Program
+    {
+        public static void ŁajlLup() 
+        {
+            int ochotnik = 1;
+            do
+            {
+                Console.WriteLine($"Liczba {ochotnik} podniesiona do kwadratu wynosi " + Math.Pow(ochotnik, 2));
+                ochotnik++;
+            } while (ochotnik <= 20);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            ŁajlLup();
+        }
+    }
+}
