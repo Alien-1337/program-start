18391bd7e649d343a01b39e85b4330e875ecf5d2
Pawel.Przybyla <pawel1996przybyla1996@outlook.com>
2024-02-04 15:19:24 +0100

    zatwierdzamy zadania 5

---
+++ CiagFibonnaciego/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Stwórz projekt w postaci aplikacji konsolowej.
+
+//Napisz program, który rekurencyjnie znajduje 10 pierwszych liczb Fibonacciego* i wyświetla je.
+
+//* Ciąg Fibonacciego to ciąg liczb naturalnych określony rekurencyjnie w następujący sposób:
+//pierwszy wyraz to F0 = 0, drugi to F1 = 1, każdy następny jest sumą dwóch poprzednich,
+// tzn. Fn-1 + Fn-2 dla n > 1. Wyrazy ciągu Fibonacciego to: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233... itd.
+
+namespace CiagFibonnaciego
+{
+class Program
+        {
+            static void Main(string[] args)
+            {
+                for (int i = 0; i < 10; i++) // W przypadku chęci wyświetlenia więcej liczb limit iteracji można zwiększyć
+                {
+                    Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
+                }
+
+            Console.ReadLine();
+        }
+
+            
+
+            static int Fibonacci(int n)
+            {
+                if (n <= 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+                }
+            }
+        }
+
+}
+
+
--- ObsługaWyjątków/Program.cs
+++ ObsługaWyjątków/Program.cs
@@ -13,39 +13,87 @@ using System.Threading.Tasks;
 
 namespace ObsługaWyjątków
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            int[,] tableOne = new int [10, 10];
+            string fileName = "dane.txt";
 
+            // Tworzenie tablicy 10x10 wypełnionej jedynkami
+            int[,] array = new int[10, 10];
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    tableOne[i, j] = 1;
+                    array[i, j] = 1;
                 }
-                 ;
             }
 
-            ZapiszDoPliku(tableOne, "data.txt");
-            Console.WriteLine("Zapisano dane do pliku");
+            // Zapisywanie tablicy do pliku
+            SaveArrayToFile(fileName, array);
 
-
+            // Odczytywanie tablicy z pliku i wyświetlanie
+            try
+            {
+                int[,] newArray = LoadArrayFromFile(fileName);
+                DisplayArray(newArray);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Plik {0} nie istnieje.", fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Wystąpił błąd podczas odczytu danych: {0}", ex.Message);
+            }
         }
 
-        static void ZapiszDoPliku(int[,] tableOne, string nazwaPliku)
+        static void SaveArrayToFile(string fileName, int[,] array)
         {
-            using (StreamWriter writer = new StreamWriter(nazwaPliku))
+            using (StreamWriter writer = new StreamWriter(fileName))
             {
-                for (int i = 0; i < tableOne.GetLength(0); i++)
+                for (int i = 0; i < 10; i++)
                 {
-                    for (int j = 0; j < tableOne.GetLength(1); j++)
+                    for (int j = 0; j < 10; j++)
                     {
-                        writer.Write(tableOne[i, j] + " ");
+                        writer.Write(array[i, j]);
+                        if (j < 9)
+                            writer.Write(" ");
                     }
                     writer.WriteLine();
                 }
+            }
+        }
+
+        static int[,] LoadArrayFromFile(string fileName)
+        {
+            int[,] newArray = new int[10, 10];
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    string line = reader.ReadLine();
+                    string[] parts = line.Split(' ');
+                    for (int j = 0; j < 10; j++)
+                    {
+                        newArray[i, j] = int.Parse(parts[j]);
+                    }
+                }
+            }
+            return newArray;
+        }
+
+        static void DisplayArray(int[,] array)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(array[i, j]);
+                    if (j < 9)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
             }
         }
     }
